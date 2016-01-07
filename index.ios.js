3f7c05c51424f8e6c908bf358b09ae3e6685ccae
Dmytro Bohachevskyy <dmytro.bohachevskyy@gmail.com>
2016-01-07 23:21:01 +0200

    Start project done

---
+++ index.ios.js
@@ -0,0 +1,129 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var formatTime = require('minutes-seconds-milliseconds');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  ListView,
+} = React;
+
+var stopwatch = React.createClass({
+  getInitialState: function() {
+    return {
+      timeElapsed: null,
+      running: false,
+      startTime: null,
+      laps: []
+    }
+  },
+
+  render: function() {
+    return (
+      <View style={[styles.container, this.border("white")]}>
+        <View style={[styles.header, this.border("yellow")]}>
+          <View style={[styles.timerView, this.border("red")]}>
+            <Text>
+            {formatTime(this.state.timeElapsed)}
+            </Text>
+          </View>
+          <View style={[styles.buttonView, this.border("green")]}>
+            <TouchableHighlight
+              underlayColor="gray"
+              onPress={this.handleStartPress}
+              style={styles.button}>
+                <Text>{this.state.running ? "Stop" : "Start"}</Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+              underlayColor="gray"
+              onPress={this.handleLapPress}
+              style={styles.button}>
+              <Text>Lap</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+        <View style={[styles.footer, this.border("blue")]}>
+          {this.listObjects()}
+        </View>
+      </View>
+    );
+  },
+
+  listObjects : function() {
+    return this.state.laps.map( function(l) {
+      return <Text>{formatTime(l)}</Text>
+    });
+  },
+
+  handleStartPress : function() {
+    if(this.state.running){
+      clearInterval(this.interval);
+      this.setState({running: false});
+      return
+    }
+
+    this.setState({startTime: new Date()});
+
+    this.interval = setInterval(() => {
+      this.setState({
+        timeElapsed: new Date() - this.state.startTime,
+        running: true
+      });
+    }, 30);
+  },
+
+  handleLapPress : function() {
+    console.log("Lap tapped");
+    this.setState({
+      startTime : new Date(),
+      laps : this.state.laps.concat([this.state.timeElapsed]),
+    })
+  },
+
+  border : function(color) {
+    return {
+      borderColor : color,
+      borderWidth : 4
+    }
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    flex: 1,
+  },
+  footer: {
+    flex: 1,
+  },
+  timerView: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonView: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  button: {
+    borderWidth: 2,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+AppRegistry.registerComponent('stopwatch', () => stopwatch);
